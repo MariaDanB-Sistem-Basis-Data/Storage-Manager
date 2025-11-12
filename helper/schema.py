f34de49fb7286b06d9e7c30c756ac74e48e9cca1
Henshou <gerrardgrs@gmail.com>
2025-11-12 16:49:51 +0700

    feat schema initializer

---
+++ helper/schema.py
@@ -0,0 +1,72 @@
+from typing import Any, Dict, List
+
+class Schema:
+    def __init__(self, attributes: List[Dict[str, Any]] = None):
+        self.attributes = attributes if attributes is not None else []
+
+    def add_attribute(self, name, type, size):
+        if any(attr['name'] == name for attr in self.attributes):
+            raise ValueError(f"Attribute '{name}' already exists in the schema.")
+        self.attributes.append({'name': name, 'type': type, 'size': size})
+
+    def get_attributes(self):
+        return self.attributes
+
+    def get_attribute(self, name):
+        for attr in self.attributes:
+            if attr['name'] == name:
+                return attr
+        raise ValueError(f"Attribute '{name}' not found in the schema.")
+    
+    def get_metadata(self):
+        return [(attr['name'], attr['type'], attr['size']) for attr in self.attributes]
+    
+    def serialize(self):
+        data = bytearray()
+        data.extend(len(self.attributes).to_bytes(2, byteorder='little'))
+
+        for attr in self.attributes:
+            name_bytes = attr['name'].encode('utf-8')
+            data.extend(len(name_bytes).to_bytes(2, byteorder='little'))
+            data.extend(name_bytes)
+            
+            type_bytes = attr['type'].encode('utf-8')
+            data.extend(len(type_bytes).to_bytes(2, byteorder='little'))
+            data.extend(type_bytes)
+
+            data.extend(attr['size'].to_bytes(2, byteorder='little'))
+        
+        return data
+    
+    def deserialize(self, data):
+        offset = 0
+        attributes = []
+
+        attr_count = int.from_bytes(data[offset:offset+2], byteorder='little')
+        offset += 2
+
+        for _ in range(attr_count):
+            name_len = int.from_bytes(data[offset:offset+2], byteorder='little')
+            offset += 2
+            name = data[offset:offset+name_len].decode('utf-8')
+            offset += name_len
+
+            type_len = int.from_bytes(data[offset:offset+2], byteorder='little')
+            offset += 2
+            type = data[offset:offset+type_len].decode('utf-8')
+            offset += type_len
+
+            size = int.from_bytes(data[offset:offset+2], byteorder='little')
+            offset += 2
+
+            attributes.append({'name': name, 'type': type, 'size': size})
+
+        return Schema(attributes)
+    
+    def __str__(self):
+        lines = ["Name".ljust(15) + "Type".ljust(10) + "Size"]
+        lines.append("-" * 32)
+        for attr in self.attributes:
+            lines.append(f"{attr['name']:<15}{attr['type']:<10}{attr['size']}")
+        return "\n".join(lines)
+        
---
+++ helper/schema_manager.py
@@ -0,0 +1,52 @@
+from .schema import Schema
+import struct
+import os
+
+class SchemaManager:
+    def __init__(self):
+        self.schemas = {}
+
+    def add_table_schema(self, table_name, schema):
+        self.schemas[table_name] = schema
+
+    def save_schemas(self):
+        path = os.path.join('data/', 'schema.dat')
+        with open(path, 'wb') as f:
+            f.write(struct.pack('i', len(self.schemas)))
+
+            for table_name, schema in self.schemas.items():
+                table_name_bytes = table_name.encode('utf-8')
+                f.write(struct.pack('i', len(table_name_bytes)))
+                f.write(table_name_bytes)
+
+                schema_data = schema.serialize()
+                f.write(struct.pack('i', len(schema_data)))
+                f.write(schema_data)
+    
+    def load_schemas(self):
+        path = os.path.join('data/', 'schema.dat')
+        self.schemas = {}
+        with open(path, 'rb') as f:
+            num_tables_bytes = f.read(4)
+            num_tables = struct.unpack('i', num_tables_bytes)[0]
+
+            for _ in range(num_tables):
+                table_name_len_bytes = f.read(4)
+                table_name_len = struct.unpack('i', table_name_len_bytes)[0]
+                table_name_bytes = f.read(table_name_len)
+                table_name = table_name_bytes.decode('utf-8')
+
+                schema_data_len_bytes = f.read(4)
+                schema_data_len = struct.unpack('i', schema_data_len_bytes)[0]
+                schema_data = f.read(schema_data_len)
+
+                schema = Schema().deserialize(schema_data)
+                self.schemas[table_name] = schema
+
+    def get_table_schema(self, table_name):
+        return self.schemas.get(table_name)
+    
+    def list_tables(self):
+        return list(self.schemas.keys())
+
+   
