3de4d81c15c06e5009f08bfadbaa448e573091ea
Henshou <gerrardgrs@gmail.com>
2025-11-13 15:46:12 +0700

    feat update record function

--- helper/slotted_page.py
+++ helper/slotted_page.py
@@ -46,3 +46,51 @@ class SlottedPage:
     def get_record(self, slot_index):  
         record_start, record_length = self.slots[slot_index]
         return bytes(self.data[record_start:record_start + record_length])
+    
+    def update_record(self, slot_index, new_record_bytes):
+        new_length = len(new_record_bytes)
+        old_start, old_length = self.slots[slot_index]
+
+        if new_length == old_length:
+            self.data[old_start:old_start + new_length] = new_record_bytes
+            return True
+        
+        if new_length < old_length:
+            diff = old_length - new_length
+            self.data[old_start:old_start + new_length] = new_record_bytes
+
+            upper_data_start = old_start + new_length
+            upper_data = self.data[upper_data_start:PAGE_SIZE]
+            self.data[old_start + new_length:PAGE_SIZE - diff] = upper_data
+
+            self.free_data_offset += diff
+
+            self.slots[slot_index] = (old_start, new_length)
+            for i, (start, length) in enumerate(self.slots):
+                if start < old_start:
+                    continue
+                self.slots[i] = (start + diff, length)
+
+            return True
+        
+        extra_space_needed = new_length - old_length
+        if self.free_data_offset - extra_space_needed < self.free_space_offset + SLOT_SIZE:
+            return False
+        
+        upper_data_start = old_start + old_length
+        upper_data = self.data[upper_data_start:PAGE_SIZE]
+        self.data[old_start + new_length:PAGE_SIZE] = upper_data
+
+        self.data[old_start:old_start + new_length] = new_record_bytes
+        self.free_data_offset -= extra_space_needed
+
+        self.slots[slot_index] = (old_start, new_length)
+        for i, (start, length) in enumerate(self.slots):
+            if start < old_start:
+                continue
+            self.slots[i] = (start - extra_space_needed, length)
+
+        return True
+
+
+
